8a49e8d8d74e61c71768fce752df758a6c247cbf
Maksym Aldokhin <maksimaldokhin84@gmail.com>
2025-02-16 17:22:50 +0200

    fix calcualtion of negative numbers

--- pages/algorithms-articles/int_representation.vue
+++ pages/algorithms-articles/int_representation.vue
@@ -34,6 +34,12 @@
         <span class="data">{{ memoryRepresentationBlocks.data }}</span>
       </div>
     </div>
+    <div v-if="steps.length">
+      <h3 class="text-md font-semibold mt-2">Розрахунок:</h3>
+      <div class="list-disc pl-5">
+        <p v-for="(step, index) in steps" :key="index" v-html="step"></p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -45,6 +51,7 @@ export default {
       memoryRepresentation: Array(4).fill('00000000'),
       errorMessage: '',
       memoryRepresentationBlocks: null,
+      steps: [],
     };
   },
   methods: {
@@ -79,18 +86,8 @@ export default {
       }
       this.errorMessage = '';
 
-      let needSetMinus = false;
-      if (decimalNumber < 0) {
-        decimalNumber = Math.abs(decimalNumber);
-        needSetMinus = true;
-      }
-
-      const binaryString = this.to32BitBinary(decimalNumber);
-      if (needSetMinus) {
-        this.memoryRepresentation = this.splitIntoBytes('1' + binaryString.slice(1));
-      } else {
-        this.memoryRepresentation = this.splitIntoBytes(binaryString);
-      }
+      let binaryString = this.calculateStepsToBinary(decimalNumber);
+      this.memoryRepresentation = this.splitIntoBytes(binaryString);
       this.memoryRepresentationBlocks = this.splitIntoComponents(this.memoryRepresentation.join(''));
     },
     isValidInteger(value) {
@@ -104,6 +101,50 @@ export default {
 
       const binary = num.toString(2);
       return binary.padStart(32, '0').slice(-32);
+    },
+    calculateSteps(number) {
+      let temp = Math.abs(number);
+      let stepResults = [];
+
+      stepResults.push(`Переводимо в двійкову систему числення число ${temp}:`);
+      while (temp > 0) {
+        let remainder = temp % 2;
+        stepResults.push(`<br>${temp} ÷ 2 = ${Math.floor(temp / 2)}, залишок = ${remainder}`);
+        temp = Math.floor(temp / 2);
+      }
+      return stepResults.reverse();
+    },
+    calculateStepsToBinary(number) {
+        let stepResult = [];
+        const isNegative = number < 0;
+
+        if (isNegative) {
+            number = Math.abs(number);
+            stepResult.push(`(${number} – змінюємо знак, бо число від’ємне)`);
+        }
+
+        stepResult.push(this.calculateSteps(number));
+        let binaryStr = number.toString(2);
+        stepResult.push(`<br>1. Перетворюємо число ${number} в двійкове: <span style="color: #D35400; font-weight: bold;">${binaryStr}</span>`);
+        binaryStr = binaryStr.padStart(32, '0');
+
+        let twosComplement = '';
+        if (isNegative) {
+            let invertedBinary = binaryStr
+                .split('')
+                .map(bit => bit === '0' ? '1' : '0')
+                .join('');
+            stepResult.push(`2. Інвертуємо біти двійкового числа: <span style="color: #007BFF; font-weight: bold;">${binaryStr} -> ${invertedBinary}</span>`);
+            let invertedBinaryTemp = invertedBinary;
+            twosComplement = (parseInt(invertedBinary, 2) + 1).toString(2);
+            stepResult.push(`3. Додаємо 1: <span style="color: #007BFF; font-weight: bold;">${invertedBinaryTemp} + 1 = ${twosComplement}</span>`);
+            stepResult.push(`4. Отримуємо доповняльний код для числа ${number}: <span style="color: #007BFF; font-weight: bold;">${twosComplement}</span>`);
+        } else {
+            twosComplement = binaryStr;
+            stepResult.push(`2. Число ${number} вже додатне, тому двійковий запис не змінюється: <span style="color: #007BFF; font-weight: bold;">${binaryStr}</span>`);
+        }
+        this.steps = stepResult;
+        return twosComplement;
     },
     splitIntoBytes(binaryString) {
       return [
