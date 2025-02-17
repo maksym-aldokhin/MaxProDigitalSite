cfba90d1f7c4a9ea46c20df91efbc44e0f15cb1b
Maksym Aldokhin <maksimaldokhin84@gmail.com>
2025-02-17 22:04:59 +0200

    add google analitics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+  devtools: {enabled: true},
   head: {
     link: [
       {
         rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&display=swap',
+        href:
+            'https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&display=swap',
       },
     ],
   },
   css: [
-    '~/assets/css/global.css', // Підключення вашого глобального CSS
+    '~/assets/css/global.css',
   ],
+  modules: ['@nuxtjs/google-gtag'],
+  runtimeConfig: {
+    public: {
+      googleAnalytics: {
+        id: 'G-V42392F0DM'
+      }
+    }
+  },
 })
